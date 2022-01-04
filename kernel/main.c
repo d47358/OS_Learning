2f67feaae645de5b372c1a7823c465394376f3ef
d47358 <1530865185@qq.com>
2022-01-04 13:49:21 +0800

    print in kernel

--- kernel/main.c
+++ kernel/main.c
@@ -1,5 +1,6 @@
 //#include "print.h"
 int main(void){
+    /*
     put_char('k');
     put_char('e');
     put_char('r');
@@ -11,5 +12,10 @@ int main(void){
     put_char('2');
     put_char('\n');
     put_char('3');
+    */
+    put_str("KERNEL PRINT\n");
+    put_int(56);
+    put_char('\n');
+    put_int(0x134);
     while(1);
 }
