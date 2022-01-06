7fdc2e47be0546cf7f688c02a145165333732edf
d47358 <1530865185@qq.com>
2022-01-06 14:09:27 +0800

    interrupt

--- kernel/main.c
+++ kernel/main.c
@@ -1,21 +1,8 @@
-//#include "print.h"
+#include "print.h"
+#include "init.h"
 int main(void){
-    /*
-    put_char('k');
-    put_char('e');
-    put_char('r');
-    put_char('n');
-    put_char('e');
-    put_char('l');
-    put_char('\n');
-    put_char('1');
-    put_char('2');
-    put_char('\n');
-    put_char('3');
-    */
-    put_str("KERNEL PRINT\n");
-    put_int(56);
-    put_char('\n');
-    put_int(0x134);
+    put_str("I AM KERNEL\n");
+    init_all();
+    asm volatile("sti");
     while(1);
 }
