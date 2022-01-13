b7fe15060b615fd2c74aa4cb09a929d0f0b887f8
d47358 <1530865185@qq.com>
2022-01-13 17:07:50 +0800

    main

--- kernel/main.c
+++ kernel/main.c
@@ -1,11 +1,16 @@
 #include "print.h"
 #include "init.h"
 #include "debug.h"
+#include "memory.h"
 int main(void){
     put_str("I AM KERNEL\n");
     init_all();
     //asm volatile("sti");
     //ASSERT(1==2);
+    void* addr=get_kernel_pages(3);
+    put_str("\n get_kernel_pages start, vaddr is:");
+    put_int((uint32_t)addr);
+    put_str("\n");
     while(1);
     return 0;
 }
